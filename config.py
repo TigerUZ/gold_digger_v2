59ad8b7df42accca728070e16feb8b2eda8551a7
TigerUZ <user@MacBook-Pro-user.local>
2025-10-07 19:08:34 +0300

    Added configuration file

---
+++ config.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Config:
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_DATABASE = os.getenv("DB_DATABASE")
+
+    def get_db_url(self):
+        """
+        Construct the database URL from the configuration.
+        """
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_DATABASE}"
+
+config = Config()
